f1f245b5c6d1da352440f38160c1e06cded5fa13
Joshua Schmidt <schmidt.uconn@gmail.com>
2019-11-28 13:16:32 -0800

    Changed appliance editing to match other edit fields

---
+++ Markd/app/src/main/java/com/schmidthappens/markd/AdapterClasses/EditApplianceRecyclerViewAdapter.java
@@ -0,0 +1,297 @@
+package com.schmidthappens.markd.AdapterClasses;
+
+import android.app.DatePickerDialog;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.schmidthappens.markd.R;
+import com.schmidthappens.markd.customer_subactivities.ApplianceEditActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class EditApplianceRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = "EditApplRecyclerAdapter";
+
+    private ApplianceEditActivity context;
+
+    public EditApplianceRecyclerViewAdapter(final ApplianceEditActivity context) {
+        this.context = context;
+    }
+
+    @Override @NonNull
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case R.layout.view_holder_edit_text:
+                return new EditTextViewHolder(LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(viewType, parent, false));
+            case 2:
+                return new DateTextViewHolder(LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.view_holder_edit_text, parent, false));
+            case R.layout.view_holder_lifespan:
+               return new LifespanViewHolder(LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(viewType, parent, false));
+            default:
+                throw new IllegalArgumentException(String.format("No view type for %d", viewType));
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        switch (position) {
+            case 0:
+                ((EditTextViewHolder) viewHolder).bindData(position, "Manufacturer", context.manufacturer);
+                break;
+            case 1:
+                ((EditTextViewHolder) viewHolder).bindData(position, "Model", context.model);
+                break;
+            case 2:
+                ((DateTextViewHolder) viewHolder).bindData(position, context.installDate);
+                break;
+            case 3:
+                ((LifespanViewHolder) viewHolder).bindData(position, context.lifespan, context.units);
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("No view holder for %d", position));
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+    @Override
+    public int getItemViewType(int position) {
+        if(position <= 1) {
+            return R.layout.view_holder_edit_text;
+        } else if(position == 2) {
+            return 2;
+        } else if(position == 3) {
+            return R.layout.view_holder_lifespan;
+        }
+        throw new IllegalArgumentException(String.format("No view holder for %d", position));
+    }
+
+    private void updateField(int fieldNumber, String newValue) {
+        switch(fieldNumber) {
+            case 0:
+                context.manufacturer = newValue;
+                break;
+            case 1:
+                context.model = newValue;
+                break;
+            case 2:
+                context.installDate = newValue;
+                break;
+            case 3:
+                try {
+                    context.lifespan = Integer.parseInt(newValue.split("\\s+")[0]);
+                    context.units = newValue.split("\\s+")[1];
+                } catch (final Exception e) {
+                    Log.e(TAG, e.toString());
+                }
+            default:
+                Log.e(TAG, String.format("No field for fieldNumber %d", fieldNumber));
+        }
+    }
+
+    class EditTextViewHolder extends RecyclerView.ViewHolder {
+        private EditText textView;
+        private int fieldNumber;
+
+        EditTextViewHolder(final View v) {
+            super(v);
+            textView = v.findViewById(R.id.edit_text_box);
+        }
+
+        void bindData(final int position, final String hint, final String currentText) {
+            fieldNumber = position;
+            textView.setHint(hint);
+            textView.setText(currentText);
+
+            textView.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    updateField(fieldNumber, s.toString());
+                }
+            });
+        }
+    }
+
+    class LifespanViewHolder extends RecyclerView.ViewHolder {
+        private EditText lifespanTextView;
+        private AutoCompleteTextView unitTextView;
+        private int fieldNumber;
+        private String timeUnitsSelected;
+
+        LifespanViewHolder(final View v) {
+            super(v);
+            lifespanTextView = v.findViewById(R.id.edit_text_box);
+            unitTextView = v.findViewById(R.id.units_dropdown);
+            unitTextView.setInputType(InputType.TYPE_NULL);
+            unitTextView.setAdapter(adapter);
+            unitTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.d(TAG, "Item Clicked: " + adapter.getItem(position));
+                    final String lifespan = lifespanTextView.getText().toString();
+                    final String timeUnits = adapter.getItem(position);
+                    if(validateValue(lifespan, timeUnits)) {
+                        timeUnitsSelected = timeUnits;
+                        updateField(fieldNumber, lifespan + " " + timeUnits);
+                        clearError();
+                    } else {
+                        setError(timeUnits);
+                    }
+                }
+            });
+            unitTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View focusedView, boolean hasFocus) {
+                    if(hasFocus) {
+                        context.hideKeyboard(focusedView);
+                    }
+                }
+            });
+        }
+
+        void bindData(final int position, final int lifespan, final String timeUnits) {
+            fieldNumber = position;
+            lifespanTextView.setText(String.valueOf(lifespan));
+            unitTextView.setText(timeUnits, false);
+            timeUnitsSelected = timeUnits;
+
+            lifespanTextView.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    final String lifespan = lifespanTextView.getText().toString();
+                    final String timeUnits = timeUnitsSelected;
+                    if(validateValue(lifespan, timeUnits)) {
+                        updateField(fieldNumber, lifespan + " " + timeUnits);
+                        clearError();
+                    } else {
+                        setError(timeUnits);
+                    }
+                }
+            });
+        }
+
+        private String[] timeUnits = new String[] {
+                "days", "months", "years"
+        };
+
+        private final ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                context, R.layout.dropdown_menu_popup_item, timeUnits
+        );
+
+        private boolean validateValue(final String lifespan, final String units) {
+            try {
+                int numberToValidate = Integer.parseInt(lifespan);
+                if (timeUnits[0].equals(units)) {
+                    return numberToValidate > 0 && numberToValidate < 366;
+                } else if (timeUnits[1].equals(units)) {
+                    return numberToValidate > 0 && numberToValidate < 13;
+                } else if (timeUnits[2].equals(units)) {
+                    return numberToValidate > 0 && numberToValidate < 51;
+                }
+
+                return false;
+            } catch (final NumberFormatException e) {
+                return false;
+            }
+        }
+
+        private void setError(final String units) {
+            String errorMessage = "Invalid Value";
+            if (timeUnits[0].equals(units)) {
+                errorMessage = "Must be between 1 and 365.";
+            } else if (timeUnits[1].equals(units)) {
+                errorMessage = "Must be between 1 and 12.";
+            } else if (timeUnits[2].equals(units)) {
+                errorMessage = "Must be between 1 and 50.";
+            }
+            lifespanTextView.setError(errorMessage);
+        }
+
+        private void clearError() {
+            lifespanTextView.setError(null);
+        }
+    }
+
+    class DateTextViewHolder extends RecyclerView.ViewHolder {
+        private EditText textView;
+        private int fieldNumber;
+        private final Calendar myCalendar = Calendar.getInstance();
+        private DatePickerDialog.OnDateSetListener date;
+
+        DateTextViewHolder(final View v) {
+            super(v);
+            textView = v.findViewById(R.id.edit_text_box);
+            textView.setClickable(true);
+            textView.setLongClickable(false);
+            textView.setFocusable(false);
+            date = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                      int dayOfMonth) {
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH, monthOfYear);
+                    myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel();
+                }
+
+            };
+        }
+
+        void bindData(final int position, final String currentText) {
+            fieldNumber = position;
+            textView.setHint("mm/dd/yyyy");
+            textView.setText(currentText);
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new DatePickerDialog(context, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+            });
+        }
+
+        private void updateLabel() {
+            final SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yy", Locale.US);
+            final String updatedDate = sdf.format(myCalendar.getTime());
+            textView.setText(updatedDate);
+            updateField(fieldNumber, updatedDate);
+        }
+    }
+}
--- Markd/app/src/main/java/com/schmidthappens/markd/customer_subactivities/ApplianceEditActivity.java
+++ Markd/app/src/main/java/com/schmidthappens/markd/customer_subactivities/ApplianceEditActivity.java
@@ -1,70 +1,53 @@
 package com.schmidthappens.markd.customer_subactivities;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.schmidthappens.markd.AdapterClasses.EditApplianceRecyclerViewAdapter;
 import com.schmidthappens.markd.R;
 import com.schmidthappens.markd.account_authentication.FirebaseAuthentication;
 import com.schmidthappens.markd.account_authentication.LoginActivity;
+import com.schmidthappens.markd.customer_menu_activities.HvacActivity;
+import com.schmidthappens.markd.customer_menu_activities.PlumbingActivity;
 import com.schmidthappens.markd.data_objects.AirHandler;
 import com.schmidthappens.markd.data_objects.Boiler;
 import com.schmidthappens.markd.data_objects.Compressor;
 import com.schmidthappens.markd.data_objects.HotWater;
 import com.schmidthappens.markd.data_objects.TempCustomerData;
-import com.schmidthappens.markd.customer_menu_activities.HvacActivity;
-import com.schmidthappens.markd.customer_menu_activities.PlumbingActivity;
-import com.schmidthappens.markd.utilities.NumberPickerUtilities;
 import com.schmidthappens.markd.utilities.StringUtilities;
-
-import java.util.Calendar;
 
 /**
  * Created by Josh on 6/6/2017.
  */
-
 public class ApplianceEditActivity extends AppCompatActivity {
-    //XML Objects
-    EditText editManufacturer;
-    EditText editModel;
-    TextView editInstallDate;
-    Button setInstallDateButton;
-    NumberPicker lifeSpanIntegerPicker;
-    NumberPicker lifeSpanUnits;
-    Button saveButton;
-
     private static final String TAG = "ApplianceEditActivity";
-    private static final String[] unitsArray = {
-            "days",
-            "months",
-            "years"
-    };
-
     private FirebaseAuthentication authentication;
     private TempCustomerData customerData;
     private boolean isContractorEditingPage;
+
+    private InputMethodManager IMM;
+
+    public String manufacturer;
+    public String model;
+    public String installDate;
+    public int lifespan;
+    public String units;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_view_plumbing);
+        IMM = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
         authentication = new FirebaseAuthentication(this);
-        initializeXMLObjects();
     }
 
     @Override
@@ -77,11 +60,17 @@ public class ApplianceEditActivity extends AppCompatActivity {
         }
         customerData = new TempCustomerData(authentication, null);
         processIntent(getIntent());
+        initializeXMLObjects();
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        if(getCurrentFocus() != null) {
+            Log.d(TAG, "Hiding Keyboard");
+            hideKeyboard(getCurrentFocus());
+        }
+        saveAppliance();
         authentication.detachListener();
         if(customerData != null) {
             customerData.removeListeners();
@@ -97,47 +86,27 @@ public class ApplianceEditActivity extends AppCompatActivity {
         return false;
     }
 
-    //Mark:- Click Listener
-    private View.OnClickListener saveButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            hideKeyboard();
-            savePlumbingChanges();
-        }
-    };
-
-    private View.OnClickListener setInstallDateButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            hideKeyboard();
-            showDatePickerDialog(v);
-        }
-    };
+    public void hideKeyboard(View view) {
+        IMM.hideSoftInputFromWindow(view.getWindowToken(),
+                InputMethodManager.RESULT_UNCHANGED_SHOWN);
+    }
 
     //Mark:- Helper Functions
     private void initializeXMLObjects() {
-        editManufacturer = (EditText)findViewById(R.id.plumbing_edit_manufacturer);
-        setEnterButtonToKeyboardDismissal(editManufacturer);
-        editModel = (EditText)findViewById(R.id.plumbing_edit_model);
-        setEnterButtonToKeyboardDismissal(editModel);
-        editInstallDate = (TextView)findViewById(R.id.plumbing_edit_install_date);
-        setInstallDateButton = (Button)findViewById(R.id.plumbing_set_install_date);
-        setInstallDateButton.setOnClickListener(setInstallDateButtonClickListener);
-
-        lifeSpanIntegerPicker = (NumberPicker)findViewById(R.id.plumbing_edit_lifespan_number_picker);
-        lifeSpanIntegerPicker.setMinValue(0);
-        lifeSpanIntegerPicker.setMaxValue(365);
-        lifeSpanIntegerPicker.setWrapSelectorWheel(false);
-        setKeyboardDismissal(lifeSpanIntegerPicker);
-
-        lifeSpanUnits = (NumberPicker)findViewById(R.id.plumbing_edit_life_span_units);
-        lifeSpanUnits.setMinValue(0);
-        lifeSpanUnits.setMaxValue(unitsArray.length-1);
-        setKeyboardDismissal(lifeSpanUnits);
-        lifeSpanUnits.setDisplayedValues(unitsArray);
-
-        saveButton = (Button)findViewById(R.id.plumbing_edit_save_button);
-        saveButton.setOnClickListener(saveButtonClickListener);
+        Log.d(TAG, "initializeXMLObjects");
+        final RecyclerView recyclerView = findViewById(R.id.edit_appliance_recycler);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(false);
+        recyclerView
+                .addItemDecoration(
+                        new DividerItemDecoration(
+                                ApplianceEditActivity.this,
+                                DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(
+                new EditApplianceRecyclerViewAdapter(this)
+        );
     }
     private void processIntent(Intent intent) {
         if(intent != null) {
@@ -145,19 +114,19 @@ public class ApplianceEditActivity extends AppCompatActivity {
                 setTitle(intent.getStringExtra("title"));
             }
             if(intent.hasExtra("manufacturer")) {
-                editManufacturer.setText(intent.getStringExtra("manufacturer"));
+                manufacturer = intent.getStringExtra("manufacturer");
             }
             if(intent.hasExtra("model")) {
-                editModel.setText(intent.getStringExtra("model"));
+                model = intent.getStringExtra("model");
             }
             if(StringUtilities.isNullOrEmpty(intent.getStringExtra("installDate"))) {
-                editInstallDate.setText(StringUtilities.getCurrentDateString());
+                installDate = StringUtilities.getCurrentDateString();
             } else {
-                editInstallDate.setText(intent.getStringExtra("installDate"));
+                installDate = intent.getStringExtra("installDate");
             }
-            lifeSpanIntegerPicker.setValue(intent.getIntExtra("lifespanInteger", 0));
+            lifespan = intent.getIntExtra("lifespanInteger", 0);
             if(intent.hasExtra("units")) {
-                NumberPickerUtilities.setPicker(lifeSpanUnits, intent.getStringExtra("units"), unitsArray);
+                units = intent.getStringExtra("units");
             }
             isContractorEditingPage = intent.getBooleanExtra("isContractor", false);
             if(isContractorEditingPage && intent.hasExtra("customerId")) {
@@ -165,37 +134,32 @@ public class ApplianceEditActivity extends AppCompatActivity {
             }
         }
     }
-    private void changeInstallDate(String newDate) {
-        editInstallDate.setText(newDate);
-    }
 
-    private void savePlumbingChanges() {
-        String manufacturer = editManufacturer.getText().toString();
-        String model        = editModel.getText().toString();
-        String installDate  = editInstallDate.getText().toString();
-        Integer lifeSpanInteger = lifeSpanIntegerPicker.getValue();
-        String units = unitsArray[lifeSpanUnits.getValue()];
-
+    private void saveAppliance() {
         Class activityToGoTo = null;
         if(getTitle().equals("Domestic Hot Water")) {
             Log.i(TAG, "Save Hot Water Changes");
-            HotWater hotWater = new HotWater(manufacturer, model, installDate, lifeSpanInteger, units);
-            customerData.updateHotWater(hotWater);
+            customerData.updateHotWater(
+                new HotWater(manufacturer, model, installDate, lifespan, units)
+            );
             activityToGoTo = PlumbingActivity.class;
         } else if(getTitle().equals("Boiler")) {
             Log.i(TAG, "Save Boiler Changes");
-            Boiler boiler = new Boiler(manufacturer, model, installDate, lifeSpanInteger, units);
-            customerData.updateBoiler(boiler);
+            customerData.updateBoiler(
+                new Boiler(manufacturer, model, installDate, lifespan, units)
+            );
             activityToGoTo = PlumbingActivity.class;
         } else if(getTitle().equals("Compressor")) {
             Log.i(TAG, "Save Compressor Changes");
-            Compressor compressor = new Compressor(manufacturer, model, installDate, lifeSpanInteger, units);
-            customerData.updateCompressor(compressor);
+            customerData.updateCompressor(
+                new Compressor(manufacturer, model, installDate, lifespan, units)
+            );
             activityToGoTo = HvacActivity.class;
         } else if(getTitle().equals("Air Handler")) {
             Log.i(TAG, "Save Air Handler Changes");
-            AirHandler airHandler = new AirHandler(manufacturer, model, installDate, lifeSpanInteger, units);
-            customerData.updateAirHandler(airHandler);
+            customerData.updateAirHandler(
+                new AirHandler(manufacturer, model, installDate, lifespan, units)
+            );
             activityToGoTo = HvacActivity.class;
         }
         goBackToActivity(activityToGoTo);
@@ -209,76 +173,5 @@ public class ApplianceEditActivity extends AppCompatActivity {
         }
         startActivity(activityIntent);
         finish();
-    }
-
-    private void setKeyboardDismissal(NumberPicker picker) {
-        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int i, int i2) {
-                hideKeyboard();
-            }
-        });
-        picker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hideKeyboard();
-            }
-        });
-    }
-
-    //Mark:- DatePickerFragment
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
-    }
-
-    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Create a new instance of DatePickerDialog and return it
-            Calendar calendar = Calendar.getInstance();
-            DatePickerDialog pickerDialog = new DatePickerDialog(getActivity(), this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-            pickerDialog.getDatePicker().setMaxDate(calendar.getTimeInMillis());
-            return pickerDialog;
-        }
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            Calendar selected = Calendar.getInstance();
-            selected.set(year, month, dayOfMonth);
-            Calendar current = Calendar.getInstance();
-
-            //Check to make sure date is not in future
-            if(selected.getTime().after(current.getTime())) {
-                Log.i(TAG, "Selected Invalid Date");
-                Toast.makeText(getActivity().getApplicationContext(), "Install date must not be in future.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            String newDate = StringUtilities.getDateString(month+1, dayOfMonth, year);
-            ((ApplianceEditActivity)getActivity()).changeInstallDate(newDate);
-        }
-    }
-
-    //Makes the enter button dismiss soft keyboard
-    private void setEnterButtonToKeyboardDismissal(final EditText view) {
-        view.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                    hideKeyboard();
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    //Hides Keyboard
-    private void hideKeyboard() {
-        View v = ApplianceEditActivity.this.getCurrentFocus();
-        if (v != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-        }
     }
 }
